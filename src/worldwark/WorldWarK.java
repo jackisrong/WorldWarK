3940480a0258b4079a600de1434d84841e79b804
justi <justi@10.10.130.117>
2018-04-20 09:25:30 -0400

    Made JFrame

--- src/worldwark/WorldWarK.java
+++ src/worldwark/WorldWarK.java
@@ -1,10 +1,31 @@
 package worldwark;
 
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
 public class WorldWarK extends JPanel {
 
     public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("World War K");
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("YEEEEEEEEEEEEEEEEEEEEEEEEEEET");
+        JMenuBar menu = new JMenuBar();
+        
+        panel.setSize(3000, 3000);
+        panel.add(label);
+        panel.add(menu);
+        
+        frame.add(panel);
+        frame.setPreferredSize(new Dimension(500, 500));
+        frame.setContentPane(panel);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
 
     }
 }
