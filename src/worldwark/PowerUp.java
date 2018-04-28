c0fdd85717cb6c19e8ab1b65f5207577463c435a
jackisrong <34490671+jackisrong@users.noreply.github.com>
2018-04-27 22:59:49 -0400

    Added power up paintComponent code.

--- src/worldwark/PowerUp.java
+++ src/worldwark/PowerUp.java
@@ -1,11 +1,18 @@
 package worldwark;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class PowerUp extends GameObject {
 
     public PowerUp(int xPos, int yPos, int width, int height) {
 	super(xPos, yPos, width, height);
+	rectangle = new Rectangle2D.Double(xPos, yPos, width, height);
     }
     
     public void update(WorldWarK panel) {
@@ -13,6 +20,23 @@ public class PowerUp extends GameObject {
     }
 
     public void paintComponent(Graphics2D g2) {
+	rectangle.setFrame(xPos, yPos, width, height);
 
+	// Draw player playerBox/hitbox
+	Color transparentColor = new Color(0, 0, 0, 0);
+	g2.setColor(transparentColor);
+	g2.fill(rectangle);
+	g2.draw(rectangle);
+
+	// Puts the player image on the player
+	BufferedImage powerUpImage;
+	try {
+	    powerUpImage = ImageIO.read(new File("assets/img/powerUp.png"));
+	} catch (IOException e) {
+	    System.out.println("ERROR: powerUp.png cannot be read.");
+	    powerUpImage = null;
+	}
+	g2.setClip(rectangle);
+	g2.drawImage(powerUpImage, xPos, yPos, null);
     }
 }
