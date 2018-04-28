d4f4cc7b80300b8ca796a04bd21e801ef16c374b
JSaurus <34481436+JSaurus@users.noreply.github.com>
2018-04-27 20:44:14 -0400

    Added enemy textures for all enemies

--- src/worldwark/Enemy.java
+++ src/worldwark/Enemy.java
@@ -18,48 +18,48 @@ public class Enemy extends GameObject {
     private int reverseTimer;
 
     public Enemy(int xPos, int yPos, int width, int height, int xSpeed, int ySpeed, int health, int typeOfEnemy) {
-	super(xPos, yPos, width, height);
-	this.xSpeed = xSpeed;
-	this.ySpeed = ySpeed;
-	this.health = health;
-	this.typeOfEnemy = typeOfEnemy;
+        super(xPos, yPos, width, height);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+        this.health = health;
+        this.typeOfEnemy = typeOfEnemy;
         reverse = false;
         reverseTimer = 0;
-	rectangle = new Rectangle2D.Double(xPos, yPos, width, height);
+        rectangle = new Rectangle2D.Double(xPos, yPos, width, height);
     }
 
     public int getXSpeed() {
-	return xSpeed;
+        return xSpeed;
     }
 
     public int getYSpeed() {
-	return ySpeed;
+        return ySpeed;
     }
 
     public void setXSpeed(int speed) {
-	xSpeed = speed;
+        xSpeed = speed;
     }
 
     public void setYSpeed(int speed) {
-	ySpeed = speed;
+        ySpeed = speed;
     }
-    
+
     public void setReverse(boolean reverse) {
         this.reverse = reverse;
     }
 
     public boolean readyToTurnAtY(int reverseYPosition) {
-	return (Math.abs(yPos - reverseYPosition) <= 3);
+        return (Math.abs(yPos - reverseYPosition) <= 3);
     }
-    
+
     public boolean readyToTurnAtX(int reverseXPosition) {
-	return (Math.abs(xPos - reverseXPosition) <= 3);
+        return (Math.abs(xPos - reverseXPosition) <= 3);
     }
-    
+
     public void update(WorldWarK panel) {
-	panel.checkEnemyCollision(this);
-        
-        if (reverse && readyToTurnAtY(panel.getHeight()/2)) {
+        panel.checkEnemyCollision(this);
+
+        if (reverse && readyToTurnAtY(panel.getHeight() / 2)) {
             ySpeed = 0;
             reverseTimer += 15;
             if (reverseTimer >= 1000) {
@@ -67,8 +67,8 @@ public class Enemy extends GameObject {
                 ySpeed = -5;
             }
         }
-        
-        if (reverse && readyToTurnAtX(panel.getWidth()/2)) {
+
+        if (reverse && readyToTurnAtX(panel.getWidth() / 2)) {
             xSpeed = 0;
             reverseTimer += 15;
             if (reverseTimer >= 1000) {
@@ -76,50 +76,67 @@ public class Enemy extends GameObject {
                 xSpeed = -5;
             }
         }
-        
+
         // Exits Left/Right wall's threshold
         if (xPos <= -201 || xPos >= 701) {
             panel.deleteObject(this);
         } else {
             xPos += xSpeed;
         }
-        
-	// Deletes the object once it has passed the bottom of the frame
-	if (yPos > panel.getHeight() - height - 1) {
-	    panel.deleteObject(this);
-	} else {
-	    yPos += ySpeed;
-	}
+
+        // Deletes the object once it has passed the bottom of the frame
+        if (yPos > panel.getHeight() - height - 1) {
+            panel.deleteObject(this);
+        } else {
+            yPos += ySpeed;
+        }
     }
 
     public void paintComponent(Graphics2D g2) {
-	rectangle.setFrame(xPos, yPos, width, height);
+        rectangle.setFrame(xPos, yPos, width, height);
 
-	// Draw enemy rectangle/hitbox
-	Color transparentColor = new Color(0, 0, 0, 0);
-	g2.setColor(transparentColor);
-	g2.fill(rectangle);
-	g2.draw(rectangle);
+        // Draw enemy rectangle/hitbox
+        Color transparentColor = new Color(0, 0, 0, 0);
+        g2.setColor(transparentColor);
+        g2.fill(rectangle);
+        g2.draw(rectangle);
 
-	// Puts appropriate enemy image on the enemy
-	BufferedImage enemyImage;
-	String fileName;
-	if (typeOfEnemy == 0) {
-	    fileName = "helicopter";
-	} else {
-	    System.out.println("ERROR: Specified enemy type doesn't have an image.");
-	    fileName = null;
-	}
+        // Puts appropriate enemy image on the enemy
+        BufferedImage enemyImage;
+        String fileName = null;
+        switch (typeOfEnemy) {
+            case 0:
+                fileName = "HelicopterLow(64X64)";
+                break;
+            case 1:
+                fileName = "HelicopterMed(64X64)";
+                break;
+            case 2:
+                fileName = "HelicopterHard(64X64)";
+                break;
+            case 3:
+                fileName = "FighterPlaneLow(64X64)";
+                break;
+            case 4:
+                fileName = "FighterPlaneMed(64X64)";
+                break;
+            case 5:
+                fileName = "FighterPlaneHard(64X64)";
+                break;
+            default:
+                break;
 
-	if (fileName != null) {
-	    try {
-		enemyImage = ImageIO.read(new File("assets/img/" + fileName + ".png"));
-	    } catch (IOException e) {
-		System.out.println("ERROR: " + fileName + ".png cannot be read.");
-		enemyImage = null;
-	    }
-	    g2.setClip(rectangle);
-	    g2.drawImage(enemyImage, xPos, yPos, null);
-	}
+        }
+
+        if (fileName != null) {
+            try {
+                enemyImage = ImageIO.read(new File("assets/img/" + fileName + ".png"));
+            } catch (IOException e) {
+                System.out.println("ERROR: " + fileName + ".png cannot be read.");
+                enemyImage = null;
+            }
+            g2.setClip(rectangle);
+            g2.drawImage(enemyImage, xPos, yPos, null);
+        }
     }
 }
